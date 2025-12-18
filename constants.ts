9c44836595e4b15713981623961a813a694891ad
stss15 <116999988+stss15@users.noreply.github.com>
2025-12-18 18:02:34 +0000

    Add 30 UK-relevant categories to game content

--- constants.ts
+++ constants.ts
@@ -36,6 +36,276 @@ export const DEFAULT_TOPICS: TopicCard[] = [
       "Fireman", "Actor", "Farmer", "Writer",
       "Baker", "Judge", "Driver", "Vet"
     ]
+  },
+  {
+    category: "UK Supermarkets",
+    words: [
+      "Tesco", "Sainsbury's", "Asda", "Waitrose",
+      "Morrisons", "Lidl", "Aldi", "Co-op",
+      "M&S", "Iceland", "Ocado", "Booths",
+      "Selfridges", "Harrods", "Spar", "Greggs"
+    ]
+  },
+  {
+    category: "BBC TV Shows",
+    words: [
+      "Doctor Who", "Sherlock", "EastEnders", "Top Gear",
+      "Strictly", "Line of Duty", "Fleabag", "The Office",
+      "Blue Planet", "Peaky Blinders", "Luther", "Ghosts",
+      "Casualty", "MasterChef", "The Traitors", "Poldark"
+    ]
+  },
+  {
+    category: "London Landmarks",
+    words: [
+      "Big Ben", "Tower Bridge", "London Eye", "The Shard",
+      "Buckingham Palace", "Royal Albert Hall", "The Gherkin", "St Paul's",
+      "Tate Modern", "Westminster Abbey", "Globe Theatre", "Hyde Park",
+      "Abbey Road", "Soho", "Covent Garden", "Trafford Centre"
+    ]
+  },
+  {
+    category: "British Chocolate",
+    words: [
+      "Dairy Milk", "KitKat", "Twirl", "Wispa",
+      "Mars Bar", "Snickers", "Crunchie", "Flake",
+      "Twix", "Bounty", "Topic", "Double Decker",
+      "Boost", "Galaxy", "Creme Egg", "Buttons"
+    ]
+  },
+  {
+    category: "UK Cities",
+    words: [
+      "London", "Manchester", "Birmingham", "Leeds",
+      "Glasgow", "Liverpool", "Edinburgh", "Bristol",
+      "Sheffield", "Newcastle", "Cardiff", "Belfast",
+      "Brighton", "Cambridge", "Oxford", "Bath"
+    ]
+  },
+  {
+    category: "British Musicians",
+    words: [
+      "The Beatles", "Adele", "Elton John", "Ed Sheeran",
+      "Dua Lipa", "Harry Styles", "Stormzy", "Coldplay",
+      "Queen", "David Bowie", "Amy Winehouse", "Lewis Capaldi",
+      "Oasis", "Spice Girls", "Arctic Monkeys", "Sam Smith"
+    ]
+  },
+  {
+    category: "British Biscuits",
+    words: [
+      "Digestive", "Hobnob", "Jammie Dodger", "Custard Cream",
+      "Bourbon", "Rich Tea", "Penguin", "Party Ring",
+      "Shortbread", "Maryland", "Chocolate Club", "Nice",
+      "Ginger Nut", "Fig Roll", "Pink Wafer", "Garibaldi"
+    ]
+  },
+  {
+    category: "London Tube Lines",
+    words: [
+      "Northern", "Central", "Piccadilly", "Victoria",
+      "Jubilee", "District", "Circle", "Bakerloo",
+      "Metropolitan", "Elizabeth", "Hammersmith", "Overground",
+      "DLR", "Waterloo & City", "Tramlink", "Cable Car"
+    ]
+  },
+  {
+    category: "Premier League Teams",
+    words: [
+      "Arsenal", "Aston Villa", "Chelsea", "Liverpool",
+      "Man City", "Man United", "Newcastle", "Spurs",
+      "Everton", "West Ham", "Brighton", "Wolves",
+      "Crystal Palace", "Brentford", "Fulham", "Luton Town"
+    ]
+  },
+  {
+    category: "British Food",
+    words: [
+      "Fish & Chips", "Sunday Roast", "Full English", "Shepherd’s Pie",
+      "Bangers & Mash", "Steak & Kidney Pie", "Toad in the Hole", "Cornish Pasty",
+      "Scotch Egg", "Pork Pie", "Trifle", "Scones",
+      "Bakewell Tart", "Eton Mess", "Sticky Toffee Pudding", "Crumpets"
+    ]
+  },
+  {
+    category: "UK Reality TV",
+    words: [
+      "Love Island", "Big Brother", "I'm A Celeb", "The Apprentice",
+      "Bake Off", "Gogglebox", "Made in Chelsea", "TOWIE",
+      "Britain's Got Talent", "X Factor", "Drag Race UK", "The Circle",
+      "First Dates", "Dinner Date", "Don't Tell the Bride", "Naked Attraction"
+    ]
+  },
+  {
+    category: "British Sitcoms",
+    words: [
+      "Fawlty Towers", "Only Fools", "Blackadder", "The Inbetweeners",
+      "Peep Show", "Father Ted", "Miranda", "Vicar of Dibley",
+      "Gavin & Stacey", "Friday Night Dinner", "Benidorm", "The Royale Family",
+      "Absolutely Fabulous", "Red Dwarf", "Spaced", "Mrs Brown's Boys"
+    ]
+  },
+  {
+    category: "UK High Street",
+    words: [
+      "Boots", "WHSmith", "Poundland", "Primark",
+      "Next", "H&M", "Waterstones", "Currys",
+      "Argos", "Wilko", "Superdrug", "JD Sports",
+      "Superdry", "River Island", "Clarks", "Tk Maxx"
+    ]
+  },
+  {
+    category: "British Animals",
+    words: [
+      "Red Squirrel", "Fox", "Badger", "Hedgehog",
+      "Otter", "Roe Deer", "Hare", "Golden Eagle",
+      "Puffin", "Seal", "Dormouse", "Pine Marten",
+      "Adder", "Stoat", "Water Vole", "Robin"
+    ]
+  },
+  {
+    category: "UK Seaside Towns",
+    words: [
+      "Blackpool", "Brighton", "Whitby", "St Ives",
+      "Scarborough", "Margate", "Bournemouth", "Southend",
+      "Skegness", "Torquay", "Eastbourne", "Tenby",
+      "Hastings", "Weymouth", "Falmouth", "Llandudno"
+    ]
+  },
+  {
+    category: "British Authors",
+    words: [
+      "Shakespeare", "Dickens", "Jane Austen", "J.K. Rowling",
+      "Roald Dahl", "Agatha Christie", "Enid Blyton", "Tolkien",
+      "George Orwell", "C.S. Lewis", "Virginia Woolf", "Terry Pratchett",
+      "Jacqueline Wilson", "David Walliams", "Philip Pullman", "Beatrix Potter"
+    ]
+  },
+  {
+    category: "UK National Parks",
+    words: [
+      "Lake District", "Peak District", "Snowdonia", "Dartmoor",
+      "Cairngorms", "New Forest", "Broads", "Loch Lomond",
+      "South Downs", "Brecon Beacons", "Exmoor", "Yorkshire Dales",
+      "North York Moors", "Northumberland", "Pembrokeshire Coast", "The Fens"
+    ]
+  },
+  {
+    category: "British Historical Figures",
+    words: [
+      "Winston Churchill", "Queen Victoria", "Henry VIII", "Florence Nightingale",
+      "Elizabeth I", "Isaac Newton", "Charles Darwin", "Horatio Nelson",
+      "Oliver Cromwell", "Isambard Brunel", "Boudica", "Mary Seacole",
+      "Stephen Hawking", "Alan Turing", "Diana, Princess of Wales", "Captain Cook"
+    ]
+  },
+  {
+    category: "British Comedians",
+    words: [
+      "Billy Connolly", "Ricky Gervais", "Peter Kay", "Dawn French",
+      "James Corden", "Michael McIntyre", "Jimmy Carr", "Lee Mack",
+      "Micky Flanagan", "Sarah Millican", "Greg Davies", "Bob Mortimer",
+      "Romesh Ranganathan", "Aisling Bea", "Joe Lycett", "Katherine Ryan"
+    ]
+  },
+  {
+    category: "UK Car Brands",
+    words: [
+      "Mini", "Land Rover", "Jaguar", "Aston Martin",
+      "Rolls-Royce", "Bentley", "McLaren", "Lotus",
+      "Vauxhall", "Morgan", "Caterham", "TVR",
+      "MG", "Triumph", "Rover", "Reliant Robin"
+    ]
+  },
+  {
+    category: "UK Podcasts",
+    words: [
+      "My Dad Wrote a Porno", "Off Menu", "The Rest Is Politics", "Sh**ged Married Annoyed",
+      "Parenting Hell", "That Peter Crouch Podcast", "Diary of a CEO", "Table Manners",
+      "Grounding", "Tailenders", "Exactly", "Happy Place",
+      "The News Agents", "Rex Factor", "Empire", "Uncanny"
+    ]
+  },
+  {
+    category: "British Inventions",
+    words: [
+      "World Wide Web", "Steam Engine", "Telephone", "Television",
+      "Radar", "Penicillin", "Jet Engine", "Vacuum Cleaner",
+      "Tarmac", "Clockwork Radio", "SMS Test", "Hovercraft",
+      "Light Bulb", "ATM", "CT Scanner", "Graphene"
+    ]
+  },
+  {
+    category: "British Sports Stars",
+    words: [
+      "Lewis Hamilton", "David Beckham", "Andy Murray", "Jessica Ennis-Hill",
+      "Mo Farah", "Tyson Fury", "Marcus Rashford", "Emma Raducanu",
+      "Wayne Rooney", "Beth Mead", "Ben Stokes", "Rory McIlroy",
+      "Chris Hoy", "Ellie Simmonds", "Tom Daley", "Adam Peaty"
+    ]
+  },
+  {
+    category: "UK Soap Characters",
+    words: [
+      "Phil Mitchell", "Dot Cotton", "Ken Barlow", "Bet Lynch",
+      "Jack Duckworth", "Deirdre Barlow", "Peggy Mitchell", "Danny Dyer",
+      "Zoe Slater", "Bianca Jackson", "Pat Butcher", "Kat Slater",
+      "Grant Mitchell", "Ian Beale", "Roy Cropper", "Steve McDonald"
+    ]
+  },
+  {
+    category: "British Afternoon Tea",
+    words: [
+      "Earl Grey", "English Breakfast", "Scones", "Clotted Cream",
+      "Strawberry Jam", "Cucumber Sandwich", "Egg & Cress", "Victoria Sponge",
+      "Macarons", "Lemon Drizzle", "Petit Fours", "Finger Sandwiches",
+      "Smoked Salmon", "Coronation Chicken", "Fruit Cake", "Sugar Cubes"
+    ]
+  },
+  {
+    category: "UK Drag Race Queens",
+    words: [
+      "The Viv", "Baga Chipz", "Lawrence Chaney", "Bimini",
+      "Tayce", "Krystal Versace", "Kitty Scott-Claus", "Danny Beard",
+      "Cheddar Gorgeous", "Ginger Johnson", "Michael Marouli", "Tomara Thomas",
+      "Blu Hydrangea", "Cheryl Hole", "Divina De Campo", "Tia Kofi"
+    ]
+  },
+  {
+    category: "UK Festivals",
+    words: [
+      "Glastonbury", "Reading", "Leeds Festival", "Wireless",
+      "Download", "Latitude", "Creamfields", "Boardmasters",
+      "Bestival", "V Festival", "T in the Park", "Parklife",
+      "Boomtown", "Green Man", "End of the Road", "Forbidden Forest"
+    ]
+  },
+  {
+    category: "British Snacks",
+    words: [
+      "Monster Munch", "Wotsits", "Quavers", "Nik Naks",
+      "Scampi Fries", "Twiglets", "Pico-ball", "Frazzles",
+      "Skips", "Squares", "Mini Cheddars", "Pork Scratchings",
+      "Hula Hoops", "McCoys", "Beef Hula Hoops", "Bacon Frazzles"
+    ]
+  },
+  {
+    category: "UK Holiday Spots (Intl)",
+    words: [
+      "Benidorm", "Magaluf", "Ibiza", "Tenerife",
+      "Lanzarote", "Algarve", "Majorca", "Costa del Sol",
+      "Zante", "Kavos", "Dubai", "New York",
+      "Orlando", "Paris", "Amsterdam", "Rome"
+    ]
+  },
+  {
+    category: "British Flowers",
+    words: [
+      "Rose", "Bluebell", "Daffodil", "Poppy",
+      "Snowdrop", "Foxglove", "Lily of the Valley", "Lavender",
+      "Daisy", "Buttercup", "Cornflower", "Primrose",
+      "Honeysuckle", "Sweet Pea", "Dahlia", "Sun Flower"
+    ]
   }
 ];
 
